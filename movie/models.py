354123aa6ebb1b1181976ddc45497e7f67e4de15
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-27 10:09:14 +0200

    create director, actor, movie models

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}, Year: {self.year}"
