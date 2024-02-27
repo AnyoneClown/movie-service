a152b7ad6ab9813b9db9f4acbdcdfa561a727a34
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-27 11:40:46 +0200

    implement scraper to fill the db

---
+++ movie/scraper.py
@@ -0,0 +1,70 @@
+from typing import NoReturn
+
+import requests
+from bs4 import BeautifulSoup
+
+from movie.models import Movie, Director, Actor
+from movie_service.settings import IMDB_API_URL, API_KEY
+
+IMDB_MOVIE_URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+
+def load_movies_data(movie_id: str) -> NoReturn:
+    url = f"{IMDB_API_URL}?apikey={API_KEY}&i={movie_id}"
+
+    data = requests.get(url).json()
+
+    title = data.get("Title")
+    year = int(data.get("Year"))
+    director_name = data.get("Director")
+    actors_names = data.get("Actors").split(", ")
+
+    print(actors_names)
+
+    director, _ = Director.objects.get_or_create(
+        first_name=director_name.split()[0], last_name=director_name.split()[1]
+    )
+
+    movie, _ = Movie.objects.get_or_create(title=title, year=year, director=director)
+
+    for actor_name in actors_names:
+        actor_parts = actor_name.split()
+
+        if len(actor_parts) == 1:
+            continue
+
+        if len(actor_parts) == 3:
+            first_name = actor_parts[0]
+            last_name = actor_parts[2]
+        else:
+            first_name, last_name = actor_parts
+
+        actor, _ = Actor.objects.get_or_create(first_name=first_name, last_name=last_name)
+
+        movie.actors.add(actor)
+
+
+def scrape_imdb() -> list[str]:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+
+    response = requests.get(IMDB_MOVIE_URL, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    movie_ids = []
+    movie_links = soup.find_all("a", class_="ipc-title-link-wrapper")
+
+    for link in movie_links:
+        href = link.get("href")
+        movie_id = href.split("/")[2]
+        movie_ids.append(movie_id)
+
+    return movie_ids[:250]
+
+
+def main() -> NoReturn:
+    movie_ids = scrape_imdb()
+
+    for movie_id in movie_ids:
+        load_movies_data(movie_id)
--- movie_service/settings.py
+++ movie_service/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
-    
     "movie",
 ]
 
@@ -125,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+IMDB_API_URL = "https://www.omdbapi.com/"
+
+API_KEY = os.getenv("API_KEY")
