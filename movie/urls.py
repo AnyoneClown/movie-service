3839063593dcfef8770e0e4ccf65c5f3305231b8
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-28 12:23:45 +0200

    update logic of views, use viewsets and routers

--- movie/urls.py
+++ movie/urls.py
@@ -1,20 +1,12 @@
-from django.urls import path
-from movie.views import (
-    DirectorListView,
-    DirectorDetailView,
-    ActorListView,
-    ActorDetailView,
-    MovieListView,
-    MovieDetailView,
-)
+from rest_framework import routers
 
-urlpatterns = [
-    path("directors/", DirectorListView.as_view(), name="director-list"),
-    path("directors/<int:pk>/", DirectorDetailView.as_view(), name="director-detail"),
-    path("actors/", ActorListView.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
-    path("movies/", MovieListView.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
-]
+from movie.views import MovieViewSet, DirectorViewSet, ActorViewSet
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("directors", DirectorViewSet)
+router.register("actors", ActorViewSet)
+
+urlpatterns = [] + router.urls
 
 app_name = "movies"
