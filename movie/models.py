5436bc01a386876f8115c52d0295c3334e422f6b
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-02 15:18:11 +0200

    update Meta class for models, add property

--- movie/models.py
+++ movie/models.py
@@ -5,6 +5,18 @@ class Director(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    @property
+    def full_name(self) -> str:
+        """Returns the full name of the Director."""
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name"]
+        verbose_name = "Director"
+        verbose_name_plural = "Directors"
+        app_label = "movie"
+        db_table = "directors"
+
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
@@ -12,6 +24,18 @@ class Director(models.Model):
 class Actor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+    @property
+    def full_name(self) -> str:
+        """Returns the full name of the actor."""
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name"]
+        verbose_name = "Actor"
+        verbose_name_plural = "Actors"
+        app_label = "movie"
+        db_table = "actors"
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -23,5 +47,15 @@ class Movie(models.Model):
     director = models.ForeignKey(Director, on_delete=models.SET_NULL, related_name="movies", null=True)
     actors = models.ManyToManyField(Actor, related_name="movies")
 
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Movie"
+        verbose_name_plural = "Movies"
+        app_label = "movie"
+        db_table = "movies"
+
     def __str__(self) -> str:
-        return f"Title: {self.title}, Year: {self.year}"
+        return (
+            f"Title: {self.title}, Year: {self.year} Director: {self.director.full_name}, "
+            f"Actors: {', '.join([actor.full_name for actor in self.actors.all()])}"
+        )
